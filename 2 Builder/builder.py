751ec93a6d25708f76f8eb8e99f1b634416d2260
Madi-S <shaiken.m@mail.ru>
2022-12-05 13:52:20 +0600

    Builder on HTML Elements example

---
+++ 2 Builder/builder.py
@@ -0,0 +1,77 @@
+
+
+class Person:
+    def __init__(self):
+        # Address
+        self.street_address = None
+        self.postcode = None
+        self.city = None
+
+        # Employment
+        self.company_name = None
+        self.position = None
+        self.annual_income = None
+
+    def __str__(self):
+        return f'Address {self.street_address} {self.postcode} {self.city}\n' + \
+            f'Employment {self.company_name} {self.position} {self.annual_income}'
+
+
+class PersonBuilder:
+    def __init__(self, person=Person()):
+        self.person = person
+        print('Person init')
+
+    @property
+    def works(self):
+        return PersonJobBuilder(self.person)
+
+    @property
+    def lives(self):
+        return PersonAdressuilder(self.person)
+
+    def build(self):
+        return self.person
+
+
+class PersonJobBuilder(PersonBuilder):
+    def __init__(self, person):
+        super().__init__(person)
+
+    def at_company(self, company_name):
+        self.person.ompany_name = company_name
+        return self
+
+    def as_a(self, position):
+        self.person.position = position
+        return self
+
+    def with_income(self, annual_income):
+        self.person.annual_income = annual_income
+        return self
+
+
+class PersonAdressuilder(PersonBuilder):
+    def __init__(self, person):
+        super().__init__(person)
+
+    def at_street(self, street_address):
+        self.person.street_address = street_address
+        return self
+
+    def with_postcode(self, postcode):
+        self.person.postcode = postcode
+        return self
+
+    def in_city(self, city):
+        self.person.city = city
+        return self
+
+
+pb = PersonBuilder()
+person = pb\
+    .lives.at_street('High Road 32').in_city('Dublin').with_postcode('023312')\
+    .works.at_company('Google').as_a('Designer').with_income(140_000)\
+    .build()
+
+print(person)
---
+++ 2 Builder/buildre_interface.py
@@ -0,0 +1,73 @@
+# HTML elements compilation example
+
+
+# Bad approach
+# text = 'abcd'
+# parts = ['<p>', text, '</p>']
+# print(''.join(parts))
+
+# words = ['hello', 'world']
+# parts = ['<ul>']
+# for w in words:
+#     parts.append(f'<li>{w}</li>')
+# parts.append('</ul>')
+# print(''.join(parts))
+
+
+class HtmlElement:
+    indent_size = 4
+
+    def __init__(self, name='', text=''):
+        self.name = name
+        self.text = text
+        self.elements = []
+
+    def __str(self):
+        lines = []
+        space = ' ' * self.indent_size
+        lines.append(f'{space}<{self.name}>')
+
+        if self.text:
+            new_space = space * 2
+            lines.append(f'{new_space}{self.text}')
+
+        for el in self.elements:
+            lines.append(el.__str())
+
+        lines.append(f'{space}</{self.name}>')
+        return '\n'.join(lines)
+
+    def __str__(self):
+        return self.__str()
+
+    @staticmethod
+    def create(name):
+        return HtmlBuilder(name)
+
+class HtmlBuilder:
+    def __init__(self, root_name):
+        self.root_name = root_name
+        self.__root = HtmlElement(root_name)
+
+    def add_child(self, child_name, text):
+        self.__root.elements.append(
+            HtmlElement(child_name, text)
+        )
+
+    def add_child_fluent(self, child_name, text):
+        self.__root.elements.append(
+            HtmlElement(child_name, text)
+        )
+        return self
+
+    def __str__(self):
+        return str(self.__root)
+
+
+# builder = HtmlBuilder('ul')
+# builder.add_child('li', 'hello')
+# builder.add_child('li', 'world')
+
+bulder = HtmlBuilder.create('ul')
+bulder.add_child_fluent('li', 'hello').add_child_fluent('li', 'world')
+print(bulder)
